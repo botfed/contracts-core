cfba3f0741e7f462e079e96c1cdd6b57aae0b1b2
David <d>
2025-11-17 13:35:26 -0300

    Revert "referrer"
    This reverts commit 2f25f6d6bab03a9fa5a661a085ce068913b1e2f8.

--- src/ReferralCodes.sol
+++ src/ReferralCodes.sol
@@ -92,9 +92,8 @@ contract ReferralCodes is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         emit CodeCreated(code, owner_, DEFAULT_CLAIMS_PER_CODE);
     }
 
-    function claimCode(string calldata codeString) external {
+    function claimCode(bytes32 code) external {
         if (referredBy[msg.sender] != bytes32(0)) revert AlreadyReferred();
-        bytes32 code = keccak256(abi.encodePacked(codeString));
         if (referralCodes[code] == 0) revert InvalidCode();
         referredBy[msg.sender] = code;
         referralCodes[code] -= 1;
